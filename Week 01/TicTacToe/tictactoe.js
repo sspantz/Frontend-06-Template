996532cecfcf2bec25dbe65695e2ae372efbbce5
sspantz <sspantiezheng@gmail.com>
2020-10-31 13:05:31 +0800

    Add feature: winner judgement

--- Week 01/TicTacToe/tictactoe.js
+++ Week 01/TicTacToe/tictactoe.js
@@ -23,8 +23,39 @@ function show(pattern) {
 
 function move(i) {
   pattern[i] = color;
-  color = 3 - color;
+  color = switchColor();
   show(pattern);
+
+  color = switchColor();
+  if (check()) {
+    alert(color === 2 ? `😆 is winer!` : `😡 is winner!`);
+  }
+  color = switchColor();
+}
+
+function switchColor() {
+  return 3 - color;
+}
+
+function check() {
+  // 8 situations
+  if (pattern[0] && pattern[0] === pattern[1] && pattern[1] === pattern[2])
+    return true;
+  if (pattern[3] && pattern[3] === pattern[4] && pattern[4] === pattern[5])
+    return true;
+  if (pattern[6] && pattern[6] === pattern[7] && pattern[7] === pattern[8])
+    return true;
+  if (pattern[0] && pattern[0] === pattern[3] && pattern[3] === pattern[6])
+    return true;
+  if (pattern[1] && pattern[1] === pattern[4] && pattern[4] === pattern[7])
+    return true;
+  if (pattern[2] && pattern[2] === pattern[5] && pattern[5] === pattern[8])
+    return true;
+  if (pattern[0] && pattern[0] === pattern[4] && pattern[4] === pattern[7])
+    return true;
+  if (pattern[2] && pattern[2] === pattern[4] && pattern[4] === pattern[6])
+    return true;
+  else return false;
 }
 
 show(pattern);
