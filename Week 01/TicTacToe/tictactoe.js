fe2a1f3a4586d855bd8c5b7ca6341e374add9971
sspantz <sspantiezheng@gmail.com>
2020-10-31 12:20:20 +0800

    Add movement feature

--- Week 01/TicTacToe/tictactoe.js
+++ Week 01/TicTacToe/tictactoe.js
@@ -1,19 +1,30 @@
-let pattern = [0, 0, 1, 1, 2, 2, 2, 0, 0];
+let pattern = [0, 0, 0, 0, 0, 0, 0, 0, 0];
 
+let color = 2;
 function show(pattern) {
   let board = document.getElementById("board");
+  board.innerHTML = "";
+
   let title = document.createElement("h1");
   title.innerText = "Tic Tac Toe    😆 vs 😡";
   board.appendChild(title);
+
   for (let i = 0; i < 9; i++) {
     let cell = document.createElement("div");
     cell.classList.add("cell");
     cell.innerText = pattern[i] === 2 ? "😆" : pattern[i] === 1 ? "😡" : "";
+    cell.addEventListener("click", () => move(i));
     board.appendChild(cell);
     if (i % 3 === 2) {
       board.appendChild(document.createElement("br"));
     }
   }
+}
+
+function move(i) {
+  pattern[i] = color;
+  color = 3 - color;
+  show(pattern);
 }
 
 show(pattern);
