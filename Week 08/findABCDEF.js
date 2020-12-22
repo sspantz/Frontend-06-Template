a8c40b94d4d4caa55d403c291a6a220bed912498
sspantz <sspantiezheng@gmail.com>
2020-12-22 14:25:02 +0800

    Complete 8.5

--- Week 08/findABCDEF.js
+++ Week 08/findABCDEF.js
@@ -1,7 +1,56 @@
 function match(string) {
+  let last;
+  let foundA = false,
+    foundB = false,
+    foundC = false,
+    foundD = false,
+    foundE = false;
+  for (let s of string) {
+    if (s === "a") {
+      foundA = true;
+      foundB = false;
+      foundC = false;
+      foundD = false;
+      foundE = false;
+      last = s;
+      continue;
+    }
+    if (s === "b" && last === "a" && foundA === true) {
+      foundB = true;
+      last = s;
+      continue;
+    }
+    if (s === "c" && last === "b" && foundB === true) {
+      foundC = true;
+      last = s;
+      continue;
+    }
+    if (s === "d" && last === "c" && foundC === true) {
+      foundD = true;
+      last = s;
+      continue;
+    }
+    if (s === "e" && last === "d" && foundD === true) {
+      foundE = true;
+      last = s;
+      continue;
+    }
+    if (s === "f" && last === "e" && foundE === true) {
+      last = s;
+      return true;
+    }
+    last = s;
+    foundA = false;
+    foundB = false;
+    foundC = false;
+    foundD = false;
+    foundE = false;
+  }
   return false;
 }
 
 console.log(match("abcdef"));
 console.log(match("abcvef"));
 console.log(match("abcddeeff"));
+console.log(match("aabbccddeeffgg"));
+console.log(match("123abcdefghi"));
