152bfdea06bde4098e72dd574d1dafe6b1f5414d
sspantz <sspantiezheng@gmail.com>
2020-12-22 14:45:08 +0800

    Create a server

---
+++ Week 08/server.js
@@ -0,0 +1,20 @@
+var http = require("http");
+var port = 4000;
+http
+  .createServer((request, response) => {
+    let body = [];
+    request
+      .on("error", error => console.log(error))
+      .on("data", chunk => {
+        console.log("chunk:", chunk);
+        body.push(chunk);
+      })
+      .on("end", () => {
+        body = Buffer.concat(body).toString();
+        console.log("body:", body);
+        response.writeHead(200, { "Content-Type": "text/html" });
+        response.end("OK!");
+      });
+  })
+  .listen(port);
+console.log(`Server is running on port ${port}`);
