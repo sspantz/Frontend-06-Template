9e7c834799c2532531c37a2c6da71fa046c6aa45
sspantz <sspantiezheng@gmail.com>
2020-12-22 15:02:12 +0800

    Update client

--- Week 08/client.js
+++ Week 08/client.js
@@ -68,13 +68,52 @@ ${this.bodyText}`;
 }
 
 class ResponseParser {
-  constructor() {}
+  constructor() {
+    this.WAIT_STATUS_LINE = 0;
+    this.WAIT_STATUS_LINE_END = 1;
+    this.WAIT_HEADER_NAME = 2;
+    this.WAIT_HEADER_SPACE = 3;
+    this.WAIT_HEADER_VALUE = 4;
+    this.WAIT_HEADER_VALUE_END = 5;
+    this.WAIT_HEADER_LINE_BLOCK_END = 6;
+    this.WAIT_BODY = 7;
+    this.statusLine = "";
+    this.headerName = "";
+    this.headerValue = "";
+    this.headers = {};
+    this.status = this.WAIT_STATUS_LINE;
+  }
   receive(string) {
     for (let i = 0; i < string.length; i++) {
       this.receiveChar(string.charAt(i));
     }
   }
-  receiveChar(c) {}
+  receiveChar(c) {
+    if (this.status === this.WAIT_STATUS_LINE) {
+      if (c === "\r") this.status = this.WAIT_STATUS_LINE_END;
+      else this.statusLine += c;
+    } else if (this.status === this.WAIT_STATUS_LINE_END) {
+      if (c === "\n") this.status = this.WAIT_HEADER_NAME;
+    } else if (this.status === this.WAIT_HEADER_NAME) {
+      if (c === ":") this.status = this.WAIT_HEADER_SPACE;
+      else if (c === "\r") this.status = this.WAIT_HEADER_LINE_BLOCK_END;
+      else this.headerName += c;
+    } else if (this.status === this.WAIT_HEADER_SPACE) {
+      if (c === " ") this.status = this.WAIT_HEADER_VALUE;
+    } else if (this.status === this.WAIT_HEADER_VALUE) {
+      if (c === "\r") {
+        this.status = this.WAIT_HEADER_VALUE_END;
+        this.headers[this.headerName] = this.headerValue;
+        this.headerName = this.headerValue = "";
+      } else this.headerValue += c;
+    } else if (this.status === this.WAIT_HEADER_VALUE_END) {
+      if (c === "\n") this.status = this.WAIT_HEADER_NAME;
+    } else if (this.status === this.WAIT_HEADER_LINE_BLOCK_END) {
+      if (c === "\n") this.status = this.WAIT_BODY;
+    } else if (this.status === this.WAIT_BODY) {
+      console.log(c);
+    }
+  }
 }
 void (async function () {
   let request = new Request({
