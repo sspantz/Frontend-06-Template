89eb2ffeffd06d97cbf685a262d9ef5e9c3f04b4
sspantz <sspantiezheng@gmail.com>
2020-12-29 19:12:10 +0800

    Update

--- Week 09/parser.js
+++ Week 09/parser.js
@@ -1,3 +1,10 @@
+const EOF = Symbol("EOF");
+
+function data(c) {}
 module.exports.parseHTML = function parseHTML(html) {
-  console.log(html);
+  let state = data;
+  for (let c of html) {
+    state = state(c);
+  }
+  state = state(EOF);
 };
