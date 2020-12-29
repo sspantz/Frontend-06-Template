f8afa57238209fbe46a43323dab82f4b54bad599
sspantz <sspantiezheng@gmail.com>
2020-12-29 19:01:01 +0800

    Fix bug: server.js

--- Week 09/server.js
+++ Week 09/server.js
@@ -1,20 +1,18 @@
 var http = require("http");
 var port = 4000;
-http
-  .createServer((request, response) => {
-    let body = [];
-    request
-      .on("error", error => console.log(error))
-      .on("data", chunk => {
-        console.log("chunk:", chunk);
-        body.push(chunk);
-      })
-      .on("end", () => {
-        body = Buffer.concat(body).toString();
-        console.log("body:", body);
-        response.writeHead(200, { "Content-Type": "text/html" });
-        response.end("OK!");
-      });
-  })
-  .listen(port);
-console.log(`Server is running on port ${port}`);
+const server = http.createServer((request, response) => {
+  let body = [];
+  request
+    .on("error", error => console.log(error))
+    .on("data", chunk => {
+      console.log("chunk:", chunk);
+      body.push(Buffer.from(chunk));
+    })
+    .on("end", () => {
+      body = Buffer.concat(body).toString();
+      console.log("body:", body);
+      response.writeHead(200, { "Content-Type": "text/html" });
+      response.end("OK!");
+    });
+});
+server.listen(port, () => console.log(`Server is running on port ${port}!`));
