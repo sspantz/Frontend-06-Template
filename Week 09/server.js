93917d3743e8473161bda101b351ab57596789e1
sspantz <sspantiezheng@gmail.com>
2020-12-30 18:37:43 +0800

    Update

--- Week 09/server.js
+++ Week 09/server.js
@@ -12,7 +12,9 @@ const server = http.createServer((request, response) => {
       body = Buffer.concat(body).toString();
       console.log("body:", body);
       response.writeHead(200, { "Content-Type": "text/html" });
-      response.end("OK!");
+      response.end(
+        `<html lang="en"><head><style>#myid {width: 100px;height: 100px;background-color: #ff5000;}.myclass {border: 1px solid green;}</style></head><body><div id="myid" class="myclass">123</div></body></html>`
+      );
     });
 });
 server.listen(port, () => console.log(`Server is listening on port ${port}!`));
